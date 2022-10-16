0b60c060244b942e1bd96a96fe2d479f27eb012c
Young Alpha <Ratig456@gmail.com>
2022-10-16 06:23:11 +0100

    basic stuff will work on in future

--- Main.cs
+++ Main.cs
@@ -1,12 +1,41 @@
-﻿namespace PortableVideoTablet
+﻿using System;
+using ABI.CCK.Components;
+using HarmonyLib;
+using MelonLoader;
+
+namespace PortableVideoTablet
 {
     public static class BuildInfo
     {
         public const string Name = "Portable Video Tablet";
         public const string Author = "Young Alpha"; 
         public const string Version = "1.0.0";
+      
     }
-    public class Main
+    public class Main:MelonMod
     {
+        public static MelonLogger.Instance Log;
+        public override void OnApplicationStart()
+        {
+            Log = LoggerInstance;
+        }
+    }
+        
+    [HarmonyPatch(typeof(CVRVideoPlayer))]
+    class CVRVideoPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void VideoPlayerStart(CVRVideoPlayer __instance)
+        {
+            try
+            {
+                
+            }
+            catch (Exception e)
+            {
+                Main.Log.Error(e);
+            }
+        }
     }
 }
