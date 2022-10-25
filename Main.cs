ab190b968282e46d0ecd2d83a3e30679f7424ba9
DDAkebono <gandvando@hotmail.com>
2022-10-25 00:18:26 -0500

    Hooked up patches, added assetbundle loading logic, initial UIX button setup

--- Main.cs
+++ Main.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ABI_RC.Core.Savior;
 using ABI.CCK.Components;
 using HarmonyLib;
 using MelonLoader;
+using UIExpansionKit.API;
+using UIExpansionKit.API.Controls;
+using UnityEngine;
 
 namespace PortableVideoTablet
 {
@@ -15,9 +22,80 @@ namespace PortableVideoTablet
     public class Main:MelonMod
     {
         public static MelonLogger.Instance Log;
+        public static MelonPreferences_Entry<bool> EnableHandle;
+        
+        internal static List<CVRVideoPlayer> VideoPlayers = new();
+
+        private IMenuToggle _toggleButton;
+        private IMenuButton _respawnButton;
+
+        //Assetbundle Stuff
+        private AssetBundle _bundle;
+        private GameObject _tabletPrefab;
+        
+        private const string PrefCategory = "PortableVideoTablet";
+
         public override void OnApplicationStart()
         {
             Log = LoggerInstance;
+            
+            Log.Msg("Starting up Portable Video Tablet!");
+
+            var category = MelonPreferences.CreateCategory(PrefCategory, "Portable Video Tablet");
+            EnableHandle = category.CreateEntry("EnableHandle", true, "Enable Tablet Handle", "Enables the handle on the video player tablet");
+
+            if (!LoadAssets())
+            {
+                Log.Error("Assetbundle failed to load! Unable to startup!");
+                return;
+            }
+            
+            if(MelonBase.RegisteredMelons.Any(x => x.Info.Name.Equals("UI Expansion Kit")))
+                SetupUIX();
+            else
+                Log.Error("You must have UIExpansionKit installed to use Portable Video Tablet!");
+        }
+
+        private void SetupUIX()
+        {
+            var uixMenu = ExpansionKitApi.GetSettingsCategory(PrefCategory);
+            uixMenu.AddLabel("Controls");
+            _toggleButton = uixMenu.AddToggleButton("Show Tablet", ToggleTablet);
+            _respawnButton = uixMenu.AddSimpleButton("Respawn Tablet", RespawnTablet);
+        }
+
+        private void ToggleTablet(bool state)
+        {
+            
+        }
+
+        private void RespawnTablet()
+        {
+            
+        }
+
+        private bool LoadAssets()
+        {
+            using (var assetStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("PortableVideoTablet.TabletAsset"))
+            {
+                if (assetStream != null)
+                {
+                    MelonDebug.Msg("[PortableVideoTablet] Loaded TabletAsset");
+
+                    using var tempStream = new MemoryStream((int)assetStream.Length);
+                    assetStream.CopyTo(tempStream);
+                    
+                    _bundle = AssetBundle.LoadFromMemory(tempStream.ToArray(), 0);
+                    _bundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+                }
+            }
+
+            if (_bundle == null) return false;
+
+            _tabletPrefab = _bundle.LoadAsset<GameObject>("tablet");
+            _tabletPrefab.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+            return true;
         }
     }
         
@@ -28,9 +106,33 @@ namespace PortableVideoTablet
         [HarmonyPostfix]
         static void VideoPlayerStart(CVRVideoPlayer __instance)
         {
+            if(CheckVR.Instance.disableVideoPlayers) return;
+            
             try
             {
+                MelonDebug.Msg($"[PortableVideoTablet] Found video player object, {__instance.name} added to list");
+
+                if (Main.VideoPlayers.Any(x => x.GetHashCode() == __instance.GetHashCode())) return;
                 
+                Main.VideoPlayers.Add(__instance);
+            }
+            catch (Exception e)
+            {
+                Main.Log.Error(e);
+            }
+        }
+
+        [HarmonyPatch("OnDestroy")]
+        [HarmonyPostfix]
+        static void VideoPlayerDestroy(CVRVideoPlayer __instance)
+        {
+            try
+            {
+                MelonDebug.Msg($"[PortableVideoTablet] Video player on destroy, {__instance.name} is being destroyed");
+                
+                if(!Main.VideoPlayers.Contains(__instance)) return;
+
+                Main.VideoPlayers.Remove(__instance);
             }
             catch (Exception e)
             {
